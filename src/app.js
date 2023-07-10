1ea40ef40760e65a6c4692d45625364850926c1d
emilynievesb <nemily1710@gmail.com>
2023-07-10 18:56:30 -0500

    feat: :sparkles: Middleware productos
    Se agregó el middleware de productos junto con su respectivo controlador

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import inventariosRoutes from "./routes/inventarios.routes.js";
 import bodegasRoutes from "./routes/bodegas.routes.js";
+import productosRoutes from "./routes/productos.routes.js";
 
 const app = express();
 app.use(express.json()); //! Middleaware para leer json
 app.use("/inventario", inventariosRoutes);
 app.use("/bodega", bodegasRoutes);
+app.use("/producto", productosRoutes);
 
 export default app;

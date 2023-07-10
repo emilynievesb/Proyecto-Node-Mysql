878c3818aff29fb2942895a4ab3c791911050097
emilynievesb <nemily1710@gmail.com>
2023-07-10 15:01:50 -0500

    feat: :sparkles: Función ejecutadora de querys
    Se creó una función para ejercutar querys de manera limpia y sin la sobrecarga de funciones bajo la premisa de que cada función tiene una sola tarea

--- src/controllers/inventarios.controllers.js
+++ src/controllers/inventarios.controllers.js
@@ -1,23 +1,35 @@
 import getConnection from "../db/connect.js";
 const connection = getConnection();
 
-//!3ER ENDPOINT, DONDE SE SUMAN LAS CANTIDADES DE PRODUCTO EN TODAS LAS BODEGAS
-const listaProductos = (req, res) => {
-  connection.query(
-    /*mysql*/ `
-    SELECT id_producto, SUM(cantidad) AS Total
-    FROM inventarios
-    GROUP BY id_producto
-    ORDER BY Total DESC
-    `,
-    (err, data, fil) => {
+//FUNCIÓN QUE EJECUTA LOS QUERYS
+const sqlQuery = (sql) => {
+  return new Promise((resolve, reject) => {
+    connection.query(sql, (err, data, fil) => {
       if (err) {
-        res.send(err);
+        reject({ status: "error", error: err });
+        console.log(err);
       } else {
-        res.json(data);
+        resolve({ status: "sucess", data: data });
+        console.log(data);
       }
-    }
-  );
+    });
+  });
+};
+
+//!3ER ENDPOINT, DONDE SE SUMAN LAS CANTIDADES DE PRODUCTO EN TODAS LAS BODEGAS
+const listaProductos = async (req, res) => {
+  try {
+    const query = /*sql*/ `
+      SELECT id_producto, SUM(cantidad) AS Total
+      FROM inventarios
+      GROUP BY id_producto
+      ORDER BY Total DESC
+      `;
+    const result = await sqlQuery(query);
+    res.send(result);
+  } catch (error) {
+    res.send(error);
+  }
 };
 
 export const methodsHTTP = {

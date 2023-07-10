eca176004e3cc0aefccdb30407e2202f5e3797be
emilynievesb <nemily1710@gmail.com>
2023-07-10 15:03:46 -0500

    feat: :sparkles: Middelware json
    Se anexó el middleware para que se reconociera el json en los bodys necesarios

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import inventariosRoutes from "./routes/inventarios.routes.js";
 import bodegasRoutes from "./routes/bodegas.routes.js";
 
 const app = express();
-
+app.use(express.json()); //! Middleaware para leer json
 app.use("/inventario", inventariosRoutes);
 app.use("/bodega", bodegasRoutes);
 
